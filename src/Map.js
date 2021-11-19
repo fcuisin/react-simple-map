aaa2f5555988b2895d053708e815dbb507dd973b
Ferdinand Cuisin <fcuisin@gmail.com>
2021-11-19 17:13:21 +0100

    display map from MapBox

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,19 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
+import mapboxgl from 'mapbox-gl';
 
-class Map extends Component {
-  render() {
-    return (
-      <p>Insert map here</p>
-    )
-  }
-}
+const Map = () => {
+  mapboxgl.accessToken = "pk.eyJ1IjoiZmN1aXNpbiIsImEiOiJjazJnZ3R0YjQwaTBwM21xZzNtMjlybXpvIn0.ver5lOt8ggHRJZ5-KFmW4g";
+
+  useEffect(() => {
+    new mapboxgl.Map({
+      container: "mapContainer",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }, []);
+
+  return <div id="mapContainer" className="map"></div>;
+};
 
 export default Map;
